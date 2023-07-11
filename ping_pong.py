527833b8ec23b1685446f18cc1332342c7052634
Εμμανουήλ Περπιράκης <139139695+ManosPerpirakes@users.noreply.github.com>
2023-07-11 16:00:03 +0300

    Update ping_pong.py
    added ball with automatic movement

--- ping_pong.py
+++ ping_pong.py
@@ -1,3 +1,4 @@
+from random import randint
 from pygame import *
 init()
 
@@ -27,15 +28,42 @@ class Playerr(SpriteGame):
         if keys_pressed[K_s] and self.rect.y < 550:
             self.rect.y += self.speed
 
+class Ball():
+    def __init__(self, x, y, width, height, image_name):
+        self.rect = rect.Rect(x, y, width, height)
+        self.rect.x = x
+        self.rect.y = y
+        self.sprite = transform.scale(image.load(image_name), (width, height))
+    def update(self):
+        global addxball
+        global addyball
+        w.blit(self.sprite, (self.rect.x, self.rect.y))
+        if self.rect.colliderect(player1.rect):
+            addxball = -(randint(3, 7))
+        if self.rect.colliderect(player2.rect):
+            addxball = randint(3, 7)
+        if self.rect.y <= 0:
+            addyball = randint(3, 7)
+        if self.rect.y >= 700:
+            addyball = -(randint(3, 7))
+        self.rect.x += addxball
+        self.rect.y += addyball
+
+        
+
 w = display.set_mode((1500, 750))
 clock = time.Clock()
 player1 = Playerl(1350, 200, 50, 200, 5, 'Image20230711154148.png')
 player2 = Playerr(100, 200, 50, 200, 5, 'Image20230711154148.png')
+ball = Ball(1000, 300, 50, 50, 'Image20230711154203.png')
 close = False
+addxball = randint(3, 7)
+addyball = randint(3, 7)
 while close != True:
     w.fill((255, 255, 255))
     player1.update()
     player2.update()
+    ball.update()
     for i in event.get():
         if i.type == QUIT:
             close = True
